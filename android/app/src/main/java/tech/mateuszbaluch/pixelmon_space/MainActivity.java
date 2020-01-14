85966ed4dd9427cf1737eb9d646e3caf5737234b
XopyIP <xopyip@gmail.com>
2020-01-14 12:42:34 +0100

    splash screen android implementation

--- android/app/src/main/java/tech/mateuszbaluch/pixelmon_space/MainActivity.java
+++ android/app/src/main/java/tech/mateuszbaluch/pixelmon_space/MainActivity.java
@@ -1,7 +1,13 @@
 package tech.mateuszbaluch.pixelmon_space;
 
+import android.graphics.drawable.Drawable;
+import android.widget.ImageView;
+
 import androidx.annotation.NonNull;
+
+import io.flutter.embedding.android.DrawableSplashScreen;
 import io.flutter.embedding.android.FlutterActivity;
+import io.flutter.embedding.android.SplashScreen;
 import io.flutter.embedding.engine.FlutterEngine;
 import io.flutter.plugins.GeneratedPluginRegistrant;
 
@@ -9,5 +15,15 @@ public class MainActivity extends FlutterActivity {
   @Override
   public void configureFlutterEngine(@NonNull FlutterEngine flutterEngine) {
     GeneratedPluginRegistrant.registerWith(flutterEngine);
+  }
+
+  @java.lang.Override
+  public SplashScreen provideSplashScreen() {
+    // Load the splash Drawable.
+    Drawable splash = getResources().getDrawable(R.drawable.splash);
+
+    // Construct a DrawableSplashScreen with the loaded splash Drawable and
+    // return it.
+    return new DrawableSplashScreen(splash, ImageView.ScaleType.CENTER, 0);
   }
 }
