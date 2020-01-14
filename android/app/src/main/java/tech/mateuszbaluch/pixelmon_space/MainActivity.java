e904eaac66864674ac512a8564f92e703169f81e
XopyIP <xopyip@gmail.com>
2020-01-14 12:50:04 +0100

    navigation button tap handlers

--- android/app/src/main/java/tech/mateuszbaluch/pixelmon_space/MainActivity.java
+++ android/app/src/main/java/tech/mateuszbaluch/pixelmon_space/MainActivity.java
@@ -19,11 +19,7 @@ public class MainActivity extends FlutterActivity {
 
   @java.lang.Override
   public SplashScreen provideSplashScreen() {
-    // Load the splash Drawable.
     Drawable splash = getResources().getDrawable(R.drawable.splash);
-
-    // Construct a DrawableSplashScreen with the loaded splash Drawable and
-    // return it.
     return new DrawableSplashScreen(splash, ImageView.ScaleType.CENTER, 0);
   }
 }
--- lib/navigation.dart
+++ lib/navigation.dart
@@ -13,58 +13,58 @@ class _NavigationState extends State<Navigation> {
     return Container(
       height: 100,
       child: Padding(
-          padding: EdgeInsets.only(left: 15, right: 15, bottom: 15, top: 30),
+        padding: EdgeInsets.only(left: 15, right: 15, bottom: 15, top: 30),
+        child: new Container(
+          decoration: BoxDecoration(
+            boxShadow: [
+              BoxShadow(
+                  offset: Offset(0, 6),
+                  blurRadius: 12,
+                  color: Color(0x60000000)),
+            ],
+            borderRadius:
+            BorderRadius.all(Radius.circular((100.0 - 30 - 15) / 2)),
+          ),
           child: new Container(
             decoration: BoxDecoration(
-              boxShadow: [
-                BoxShadow(
-                    offset: Offset(0, 6),
-                    blurRadius: 12,
-                    color: Color(0x60000000)),
-              ],
-              borderRadius: BorderRadius.all(Radius.circular((100.0 - 30 - 15) / 2)),
+              borderRadius:
+              BorderRadius.all(Radius.circular((100.0 - 30 - 15) / 2)),
+              color: Color(0xffeaeaea),
             ),
-            child: new Container(
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.all(Radius.circular((100.0 - 30 - 15) / 2)),
-                color: Color(0xffeaeaea),
-              ),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: <Widget>[
-                  Container(
-                    child: SizedBox(
-                      height: 100,
-                      child: Image(image: AssetImage("assets/pokedex.png")),
-                    ),
-                    transform: Matrix4.translationValues(-5, -20, 0)..scale(1.2),
-                  ),
-                  Container(
-                    child: SizedBox(
-                      height: 100,
-                      child: Image(image: AssetImage("assets/biome_disabled.png")),
-                    ),
-                    transform: Matrix4.translationValues(-5, -20, 0)..scale(1.2),
-                  ),
-                  Container(
-                    child: SizedBox(
-                      height: 100,
-                      child: Image(image: AssetImage("assets/drop_disabled.png")),
-                    ),
-                    transform: Matrix4.translationValues(-5, -20, 0)..scale(1.2),
-                  ),
-                  Container(
-                    child: SizedBox(
-                      height: 100,
-                      child: Image(image: AssetImage("assets/crafting_disabled.png")),
-                    ),
-                    transform: Matrix4.translationValues(-5, -20, 0)..scale(1.2),
-                  ),
-                ],
-              ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: <Widget>[
+                createBox("assets/pokedex.png", () {
+                  print("dex");
+                }),
+                createBox("assets/biome_disabled.png", () {
+                  print("biome");
+                }),
+                createBox("assets/drop_disabled.png", () {
+                  print("drop");
+                }),
+                createBox("assets/crafting_disabled.png", () {
+                  print("crafting");
+                }),
+              ],
             ),
           ),
+        ),
       ),
+    );
+  }
+
+  createBox(String image, Null Function() handler) {
+    return GestureDetector(
+      child: Container(
+        child: SizedBox(
+          height: 100,
+          child: Image(image: AssetImage(image)),
+        ),
+        transform: Matrix4.translationValues(-5, -20, 0)
+          ..scale(1.2),
+      ),
+      onTap: handler,
     );
   }
 }
