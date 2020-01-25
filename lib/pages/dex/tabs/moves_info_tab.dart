84559ec6d8a9ec65a2c52bb4d7fb71ab8dd9143b
XopyIP <xopyip@gmail.com>
2020-01-25 20:27:45 +0100

    last moves tables

--- lib/pages/dex/tabs/moves_info_tab.dart
+++ lib/pages/dex/tabs/moves_info_tab.dart
@@ -11,9 +11,7 @@ class MovesInfoTab extends StatelessWidget {
     return ListView(
       children: <Widget>[
         Table(
-          columnWidths: {
-            0: IntrinsicColumnWidth()
-          },
+          columnWidths: {0: IntrinsicColumnWidth()},
           border: TableBorder(
             verticalInside: BorderSide(color: Color(0xffdddddd)),
             horizontalInside: BorderSide(color: Color(0xffdddddd)),
@@ -50,40 +48,120 @@ class MovesInfoTab extends StatelessWidget {
             ]),
           ]..addAll(pokemon.levelUpMoves.keys.map((level) {
             return pokemon.levelUpMoves[level]
-                .map((move) => TableRow(children: [
-              TableCell(
-                child: Padding(
-                  padding: const EdgeInsets.all(8),
-                  child: Text(
-                    level,
-                    textAlign: TextAlign.end,
+                .map((move) =>
+                TableRow(children: [
+                  TableCell(
+                    child: Padding(
+                      padding: const EdgeInsets.all(8),
+                      child: Text(
+                        level,
+                        textAlign: TextAlign.end,
+                      ),
+                    ),
                   ),
-                ),
-              ),
-              TableCell(
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Text(move.attackName),
-                ),
-              ),
-              TableCell(
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Text(move.attackCategory),
-                ),
-              ),
-              TableCell(
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Text(move.attackType),
-                ),
-              ),
-            ]))
+                  TableCell(
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text(move.attackName),
+                    ),
+                  ),
+                  TableCell(
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text(move.attackCategory),
+                    ),
+                  ),
+                  TableCell(
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text(move.attackType),
+                    ),
+                  ),
+                ]))
                 .toList();
           }).reduce((a, b) => a..addAll(b))),
         ),
-        SizedBox(height: 50,)
+        Padding(
+          padding: const EdgeInsets.only(top: 30.0, bottom: 6),
+          child: Text("Ataki TM",
+            style: TextStyle(fontSize: 22, color: Color(0xff767676)),
+            textAlign: TextAlign.center,),
+        ),
+        buildTable(pokemon.tmMoves),
+        Padding(
+          padding: const EdgeInsets.only(top: 30.0, bottom: 6),
+          child: Text("Ataki Tutora",
+            style: TextStyle(fontSize: 22, color: Color(0xff767676)),
+            textAlign: TextAlign.center,),
+        ),
+        buildTable(pokemon.tutorMoves),
+        Padding(
+          padding: const EdgeInsets.only(top: 30.0, bottom: 6),
+          child: Text("Ataki z hodowli",
+            style: TextStyle(fontSize: 22, color: Color(0xff767676)),
+            textAlign: TextAlign.center,),
+        ),
+        buildTable(pokemon.eggMoves),
+        SizedBox(
+          height: 50,
+        )
       ],
+    );
+  }
+
+  buildTable(List<PokeMove> moves) {
+    return Table(
+      border: TableBorder(
+        verticalInside: BorderSide(color: Color(0xffdddddd)),
+        horizontalInside: BorderSide(color: Color(0xffdddddd)),
+      ),
+      children: [
+        TableRow(children: [
+          TableCell(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text("Atak"),
+            ),
+          ),
+          TableCell(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text("Kategoria"),
+            ),
+          ),
+          TableCell(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text("Typ"),
+            ),
+          ),
+        ]),
+      ]
+        ..addAll(
+          moves
+              .map((move) =>
+              TableRow(children: [
+                TableCell(
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text(move.attackName),
+                  ),
+                ),
+                TableCell(
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text(move.attackCategory),
+                  ),
+                ),
+                TableCell(
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text(move.attackType),
+                  ),
+                ),
+              ]))
+              .toList(),
+        ),
     );
   }
 }
